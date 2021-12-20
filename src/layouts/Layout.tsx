0d1a12da50cc09cd5e2416d5b3a6f3cd40ecf178
Masha39 <zaitcevamasha39@gmail.com>
2021-12-20 14:59:38 +0400

    Prettier, eslint config. Quick type fix on Layout props

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
-
 import styles from './Layout.module.scss'
 import Header from '../components/Header/Header'
 
-const Layout = ({children}) => (
-    <div className={styles.main}>
-     
-      <Header />
-      
-      <div className={styles.content}>{children}</div>
-      <Footer/> 
-    </div>
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <div className={styles.main}>
+    <Header />
+
+    <div className={styles.content}>{children}</div>
+    <Footer />
+  </div>
 )
 
 export default Layout
