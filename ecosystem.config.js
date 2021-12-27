ed8def6a50a07eaacc559aa98002693c438568fb
Vitaly Galaichuk <swissarmytowel@gmail.com>
2021-12-27 17:03:58 +0300

    Temporary change branches

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,13 +18,13 @@ module.exports = {
       user: 'marvin',
       host: 'api.42.works',
       key: 'deploy.key',
-      ref: 'origin/develop',
+      ref: 'origin/feature/ci-cd',
       repo: 'git@gitlab.com:your-care-plus/frontend.git',
       ssh_options: ['StrictHostKeyChecking=no', 'PasswordAuthentication=no'],
       fetch: '--all',
       path: '/home/marvin/yourcareplus/frontend',
       'post-deploy':
-        'git checkout develop && npm install --no-optional && npm run build && pm2 reload ecosystem.config.js --env staging'
+        'git checkout feature/ci-cd && npm install --no-optional && npm run build && pm2 reload ecosystem.config.js --env staging'
     }
   }
 }
