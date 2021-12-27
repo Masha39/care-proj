41a83f54c7e440d43e52010097b9edba9d366a52
Vitaly Galaichuk <swissarmytowel@gmail.com>
2021-12-27 17:12:41 +0300

    Change set port directive

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,7 @@ module.exports = {
     {
       name: 'yourcareplus-frontend',
       script: 'npm',
-      args: 'run start -p 3008',
-      time: true,
-      instances: 1,
-      autorestart: true,
-      max_restarts: 50,
-      watch: false,
-      max_memory_restart: '500M'
+      args: 'run start -- -p 3008'
     }
   ],
 
