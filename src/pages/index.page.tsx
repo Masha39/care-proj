8c03d0c36fe0469e29b734654b5ef995b71b4c10
Masha39 <zaitcevamasha39@gmail.com>
2022-01-21 18:40:51 +0400

    Button small improvements

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import { Button } from 'components/button/button'
 import { LandingBanner } from 'components/main_page/landing-banner/landing_banner'
 import { MainPageContent } from 'components/main_page/main_page_content'
 import Layout from 'layouts/layout'
@@ -15,6 +16,37 @@ const Home: NextPage = () => {
       <Layout>
         <LandingBanner />
         <MainPageContent />
+        <Button
+          buttonStyle="primary_blue"
+          title="primary_blue"
+          onClick={() => {}}
+        />
+        <Button
+          buttonStyle="primary_orange"
+          size="big"
+          title="primary_orange big"
+          onClick={() => {}}
+        />
+        <Button buttonStyle="secondary" title="secondary" onClick={() => {}} />
+        <Button
+          buttonStyle="destructive"
+          title="destructive"
+          onClick={() => {}}
+        />
+        <Button buttonStyle="tertiary" title="tertiary" onClick={() => {}} />
+        <Button
+          buttonStyle="destructive_small"
+          title="destructive_small"
+          onClick={() => {}}
+        />
+        <Button buttonStyle="add" title="add" onClick={() => {}} />
+        <Button buttonStyle="add_small" title="add_small" onClick={() => {}} />
+        <Button buttonStyle="icon" title="icon" onClick={() => {}} />
+        <Button
+          buttonStyle="icon_filled"
+          title="icon_filled"
+          onClick={() => {}}
+        />
       </Layout>
     </>
   )
