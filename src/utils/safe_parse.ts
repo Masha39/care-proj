b7c980c9ae51a6c862abffbc44794a1a7acc1291
Masha39 <zaitcevamasha39@gmail.com>
2022-01-25 20:35:27 +0400

    Removed --no-optional from ci config

--- src/utils/safe_parse.ts
+++ src/utils/safe_parse.ts
@@ -1,7 +1,7 @@
 import parse from 'html-react-parser'
-// import DOMPurify from 'isomorphic-dompurify'
+import DOMPurify from 'isomorphic-dompurify'
 
 export const safeParse = (value: string) => {
-  // const clean = DOMPurify.sanitize(value, { ALLOWED_TAGS: ['b', 'a'] })
-  return parse(value)
+  const clean = DOMPurify.sanitize(value, { ALLOWED_TAGS: ['b', 'a'] })
+  return parse(clean)
 }
