7d9d4ee417e4cd8c816acd429bfe8172e1545f74
“Victor <justicejesus1237@gmail.com>
2022-02-04 15:02:49 +0300

    refactor privacy_policy is done

--- src/pages/privacy-policy/index.page.tsx
+++ src/pages/privacy-policy/index.page.tsx
@@ -4,7 +4,7 @@ import useTranslation from 'next-translate/useTranslation'
 
 import Layout from 'layouts/layout'
 
-import { PrivacyPolicyContent } from './privacy_policy_content'
+import styles from './privacy_policy.module.scss'
 
 export type TypePolicy = {
   title: string
@@ -21,11 +21,28 @@ const PrivacyPolicy = () => {
     { count: 1 },
     { returnObjects: true }
   )
+
   return (
     <Layout>
-      {policyData.map((item, index) => (
-        <PrivacyPolicyContent item={item} key={index} />
-      ))}
+      <div className="container">
+        {policyData.map((item, index) => (
+          <div key={index}>
+            <h1 className={styles.h1}>{item.h1}</h1>
+            <p className={styles.title}>{item.title}</p>
+            <p>{item.description}</p>
+            <ul className={styles.list}>
+              {item.list
+                ? item.list.map((el, i) => (
+                    <li className={styles.list_item} key={i}>
+                      {el}
+                    </li>
+                  ))
+                : null}
+            </ul>
+            <p>{item.descriptionAdd}</p>
+          </div>
+        ))}
+      </div>
     </Layout>
   )
 }
